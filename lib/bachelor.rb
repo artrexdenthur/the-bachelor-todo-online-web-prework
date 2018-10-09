7123aae30359af36691876d95fe212e50d99863d
Paul Ashour <paul.ashour@gmail.com>
2018-10-09 14:21:46 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   # looking for { status: "Winner" }
   # data[season] is an array of hashes
-  data[season].find { |contestant| contestant.fetch(:status) == 'Winner' }
+  data[season].find { |contestant| contestant.fetch('status') == 'Winner' }
 end
 
 def get_contestant_name(data, occupation)

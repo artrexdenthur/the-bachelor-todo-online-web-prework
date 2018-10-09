1d3c89ab489810245bbb63ade2d3328541cc5d4c
Paul Ashour <paul.ashour@gmail.com>
2018-10-09 14:45:07 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -24,7 +24,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant|
     total_age += contestant['age'].to_f
   end
-  total_age/data[season].count
+  (total_age/data[season].count).round
 end
 
 def all_contestants(data)

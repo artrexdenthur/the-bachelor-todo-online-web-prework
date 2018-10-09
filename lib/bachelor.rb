ff69ae830642072b6f749e3ea4708c15cad088c3
Paul Ashour <paul.ashour@gmail.com>
2018-10-09 14:32:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,7 +11,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  all_contestants(data).count { |contestant| contestant.fetch('hometown') == hometown }
 end
 
 def get_occupation(data, hometown)
